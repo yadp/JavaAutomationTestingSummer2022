10058503c32f2ce48602446d6b4add22b7c47914
Yavor Petkov <yavorpetkov92@gmail.com>
2022-06-10 09:49:37 +0300

    Add Lecture 1

---
+++ Lecture1/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String message = "Hello world!!!";
+        System.out.println(message);
+        System.out.println("Second Line");
+
+        String name = "Yavor:";
+
+        int age = 29;
+
+        System.out.println(name + age);
+
+        byte tooBig = 127;
+
+        System.out.println(tooBig);
+
+        tooBig = 120;
+
+        System.out.println(tooBig);
+
+        tooBig = (byte) (tooBig + 10);
+
+        System.out.println(tooBig);
+
+        int number1 = 5;
+        int number2 = 6;
+        number1 =  5 + 6;
+
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+}
